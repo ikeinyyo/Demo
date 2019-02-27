a2de89afadd53ede6604342fdf816c135ee9369c
Sergio Gallardo Sales <sgallardo@plainconcepts.com>
2019-02-27 22:43:05 +0100

    Add Pot to calc

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from utils.args import get_argument, get_integer_argument
 from providers.fib import fib
 from providers.factorial import factorial
-from providers.calcs import mult
+from providers.calcs import mult, pot
 
 def main(argv):
     method = get_argument(argv, 0)
@@ -12,6 +12,8 @@ def main(argv):
         factorial(get_integer_argument(argv, 1))
     elif method == "mult":
         mult(get_integer_argument(argv, 1), get_integer_argument(argv, 2))
+    elif method == "pot":
+        pot(get_integer_argument(argv, 1), get_integer_argument(argv, 2))
     else:
         print("The method is not valid")
 
--- providers/calcs.py
+++ providers/calcs.py
@@ -6,3 +6,16 @@ def calc_mult(a, b):
         return 0
     else:
         return a + calc_mult(a,b-1)
+
+def pot(b,n):
+    print(calc_pot(b,n))
+
+def calc_pot(b,n):
+    if n <= 0:
+        return 1
+    if n % 2 == 0:
+        pot = calc_pot(b, n/2)
+        return calc_mult(pot, pot)
+    else:
+        pot = calc_pot(b, (n-1)/2)
+        return calc_mult(calc_mult(pot, pot), b)
