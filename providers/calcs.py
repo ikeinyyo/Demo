16260e4c0aefcd2e4966e6d71acf94aee3b23f9d
Sergio Gallardo Sales <sgallardo@plainconcepts.com>
2019-02-27 23:55:44 +0100

    Change pot method using div

--- providers/calcs.py
+++ providers/calcs.py
@@ -14,17 +14,17 @@ def calc_pot(b,n):
     if n <= 0:
         return 1
     if n % 2 == 0:
-        pot = calc_pot(b, n/2)
+        pot = calc_pot(b, calc_div(n,2))
         return calc_mult(pot, pot)
     else:
-        pot = calc_pot(b, (n-1)/2)
+        pot = calc_pot(b, calc_div((n-1),2))
         return calc_mult(calc_mult(pot, pot), b)
 
 def div(a,b):
     print(calc_div(a, b))
 
 def calc_div(a,b):
-    if a < b:
+    if a <= b:
         return 0
     else:
         return 1 + calc_div(a-b,b)
@@ -36,7 +36,7 @@ def calc_div(a,b):
     else:
         return 1 + calc_div(a-b,b)
     '''
-    
+
 def mod(a, b):
     print(calc_mod(a,b))
 
